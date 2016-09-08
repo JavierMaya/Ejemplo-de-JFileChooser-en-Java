b97afefef50558f8d4421775ab006698ed9a049c
JavierMaya <javiermayaparedes@outlook.com>
2016-09-08 00:36:11 -0500

    19
    Proyecto Java Javier Maya

---
+++ EjemploFileChooser/src/ejemplofilechooser/EjemploFileChooser.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplofilechooser;
+
+
+ import Vista.*;
+
+public class EjemploFileChooser {
+
+   
+    public static void main(String[] args)  {
+        
+      Vista v=new Vista();
+      
+      v.setVisible(true);
+      
+      
+    }
+    
+}
